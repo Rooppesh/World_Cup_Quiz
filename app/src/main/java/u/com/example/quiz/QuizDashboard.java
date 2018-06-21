75e5d8d6d99a1ad0b0684dd4c1f8e9e4af3acbf5
User1 <rooppesh98@gmail.com>
2018-06-21 15:22:33 +0530

    Initial commit

---
+++ app/src/main/java/u/com/example/quiz/QuizDashboard.java
@@ -0,0 +1,43 @@
+package u.com.example.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class QuizDashboard extends AppCompatActivity {
+
+    public CardView cric,tech,topscore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_quiz_dashboard);
+        cric = (CardView)findViewById(R.id.cricketquiz);
+        topscore = (CardView)findViewById(R.id.topscore);
+        cric.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),CricketQuiz.class);
+                startActivityForResult(i,1);
+
+            }
+        });
+
+        topscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivityForResult(i,1);
+
+            }
+        });
+    }
+
+}
